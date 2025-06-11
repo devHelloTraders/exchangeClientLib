2baa4d327b1bce3b609dc8b6842b2cdaf577493f
bnechlani09 <bnechlani09@gmail.com>
2025-06-11 13:20:22 +0530

    1. TransactionUpdateRecord change.

--- src/main/java/com/traders/exchange/domain/TransactionStatus.java
+++ src/main/java/com/traders/exchange/domain/TransactionStatus.java
@@ -2,13 +2,7 @@ package com.traders.exchange.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.time.LocalDateTime;
-
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum TransactionStatus {
     PENDING, COMPLETED, CANCELLED;
-
-    public LocalDateTime completedTime() {
-        return this == COMPLETED ? LocalDateTime.now() : null;
-    }
 }
